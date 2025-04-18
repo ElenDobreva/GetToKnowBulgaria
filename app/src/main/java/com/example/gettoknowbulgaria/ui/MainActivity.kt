c4175047c19665155f3e755a12a59c1d539d92f0
ElenDobreva <edobreva03@gmail.com>
2025-04-18 23:29:53 +0300

    Създадена проекта структура!

--- app/src/main/java/com/example/gettoknowbulgaria/ui/MainActivity.kt
+++ app/src/main/java/com/example/gettoknowbulgaria/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.gettoknowbulgaria
+package com.example.gettoknowbulgaria.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gettoknowbulgaria.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/gettoknowbulgaria/ui/ProfileActivity.kt
@@ -0,0 +1,4 @@
+package com.example.gettoknowbulgaria.ui
+
+class ProfileActivity {
+}
